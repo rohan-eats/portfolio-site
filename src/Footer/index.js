42134cb8dbbf871a557b28d3bbefb914ab97e1e9
rohan-eats <54456208+rohan-eats@users.noreply.github.com>
2021-10-15 11:42:33 +0530

    footer update and main update pages

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -4,15 +4,9 @@ import './style.scss';
 export default function Footer() {
     return (
         <div className="footer-container">
-            <div>
                 <a href="mailto:sales@hittitestechnology.com">sales@hittitestechnology.com</a>
-            </div>
-            <div>
                 <p>© {new Date().getFullYear()} Hittites Technology India Limited. | All Rights Reserved</p>
-            </div>
-            <div>
                 <a href="tel:+919620018180"> +91 962-001-8180</a>
-            </div>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { gsap } from "gsap";
-
-import CustomCursor from '../components/CustomCursor';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import Seo from '../components/seo';
 import Hero from '../components/Hero/Hero';
@@ -20,18 +14,6 @@ export default function Index() {
 
   const [loader, setLoader] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 800);
-  }, []);
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-
-  }, [])
-
 
   return loader ? <>Loading</> :
     (
@@ -40,11 +22,8 @@ export default function Index() {
 
         <CustomCursor />
 
-        <div className="main-container"
-          id="main-container"
-        >
-          <Hero />
-          <About />
+        <div className="main-container" >
+
           <FeatureSlides />
           <Testimonials />
           <Contact />
